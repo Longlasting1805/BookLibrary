dfbef01cf259d390d5f2c8410101330c4a38d4fe
Akande Al-ameen <longlasting1805@gmail.com>
2021-12-04 07:02:20 +0000

    first commit

---
+++ catalog/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+
+from .models import Author, Genre, Book, BookInstance
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'dob', 'dod')
+
+
+admin.site.register(Author, AuthorAdmin)
+
+admin.site.register(Genre)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
+# Register the admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
+
+# class BookInstanceInline(admin.TabularInline):
+#     model = BookInstance
+
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'display_genre')
+#
+#     inlines = [BookInstance]
