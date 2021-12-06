749ffacadfbd7f09d15b02f2407f0772ee575a49
Akande Al-ameen <longlasting1805@gmail.com>
2021-12-06 10:17:01 +0000

    templating

--- book_library/settings.py
+++ book_library/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'book_library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('catalog/', include('catalog.urls')),
+    # path('catalog/', include('catalog.urls')),
     path('', views.index, name='index'),
+
 ]
