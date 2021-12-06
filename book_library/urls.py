7c90a79e6c379a61dab6491bb064d1afcfbe283b
Akande Al-ameen <longlasting1805@gmail.com>
2021-12-06 14:32:07 +0000

    book_detail.html

--- book_library/urls.py
+++ book_library/urls.py
@@ -20,10 +20,14 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('catalog/', include('catalog.urls')),
-                  path('books/', views.BookListView.as_view(), name='books'),
-                  path('', RedirectView.as_view(url='catalog/', permanent=True)),
+from catalog import views
 
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
@@ -7,9 +8,18 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
     def get_queryset(self):
         return Book.objects.filter(title__icontains='war')[:5]  # Get 5 books containing the title war
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog.book_detail.html', context={'book': book})
 
 
 def index(request):
@@ -22,6 +32,8 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'new_books': num_books,
@@ -32,3 +44,5 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
