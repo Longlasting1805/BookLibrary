79749e3013e03854cd82e050ff0c7d688e082a22
Akande Al-ameen <longlasting1805@gmail.com>
2021-12-06 15:19:30 +0000

    registration template

--- book_library/urls.py
+++ book_library/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import login
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
-from catalog import views
+from catalog import views, urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
